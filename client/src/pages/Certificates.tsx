54037dc1f3185de966aac8ed63ffa22f2c3b6580
Brunda reddy <ybrundareddy2006@gmail.com>
2026-06-13 19:38:50 +0530

    Update Certificates.tsx

--- client/src/pages/Certificates.tsx
+++ client/src/pages/Certificates.tsx
@@ -5,13 +5,21 @@ import { motion } from "framer-motion";
 import { Card, CardContent } from "@/components/ui/card";
 
 const localCertificates = [
+   {
+  id: 1,
+  title: "ServiceNow Certified Application Developer (CAD)",
+  issuer: "ServiceNow",
+  date: "May 2026",
+  link: "/cad.pdf",
+  imageUrl: "/cad.png",
+},
   {
   id: 1,
   title: "ServiceNow Certified System Administrator (CSA)",
   issuer: "ServiceNow",
   date: "Apr 2026",
   link: "/servicenow-csa.pdf",
-  imageUrl: "https://scontent.ftir6-1.fna.fbcdn.net/v/t39.30808-6/272186583_10161055474847573_8546211691609073158_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=1d70fc&_nc_ohc=Fmf1z-SJcfsQ7kNvwFGXjv4&_nc_oc=AdoItZoenHaq8Yp5T6a1MTO_JLKxo_kQYWASAh5duTo4gBtoR8Cz8T3KynrjxVjirjw&_nc_zt=23&_nc_ht=scontent.ftir6-1.fna&_nc_gid=csyFKQqVUKuzjIywcmE74w&_nc_ss=7b2a8&oh=00_Af5C3T_E6I3i2HngVKGeiKrC9ks8XjXlNHIoINMOelw53Q&oe=69FB91D6",
+  imageUrl: "/csa.png",
 },
   {
     id: 2,
